cc1f0db0b4579ef1b7e9db91ef6675c90fe0516c
liuzhibing <sw.dabing@hotmail.com>
2019-05-31 16:15:19 +0800

    1.0.0

--- src/view/components/tables/camera.vue
+++ src/view/components/tables/camera.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Card>
-      <iframe style="width:1284px;height:724px" src="http://raspberrypi:8080/?action=stream"></iframe>
+      <!-- <iframe style="width:1284px;height:724px" src="http://raspberrypi:8080/?action=stream"></iframe> -->
+      <iframe style="width:1300px;height:740px" src="http://raspberrypi:8080/javascript_simple.html"></iframe>
     </Card>
   </div>
 </template>
