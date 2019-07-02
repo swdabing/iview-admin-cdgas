2cd556e85da6722ba0d31df7462f9dd4c7861322
liuzhibing <sw.dabing@hotmail.com>
2019-07-02 10:05:04 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -25,19 +25,20 @@ module.exports = {
   // https://www.foobar.com/my-app/
   // then change this to '/my-app/'
   publicPath: BASE_URL,
+
   // tweak internal webpack configuration.
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   // 如果你不需要使用eslint，把lintOnSave设为false即可
   lintOnSave: true,
+
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('_c', resolve('src/components'))
   },
-  // 设为false打包时不生成.map文件
-  productionSourceMap: false
-  // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
 
+  // 设为false打包时不生成.map文件
+  // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   // devServer: {
   //   disableHostCheck: true, // 通过域名访问
   //   proxy: // 'http://cis.crcgas.com/cis/'
@@ -52,4 +53,7 @@ module.exports = {
   //           }
   //         }
   // }
+  productionSourceMap: false,
+
+  outputDir: 'cinp'
 }
